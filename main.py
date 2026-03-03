a6c08770c2e72f554eac8b7fc6b4683750b00c3d
Muhammad Ukasha <1718ukashae2@gmail.com>
2026-03-03 11:10:43 +0500

    new model

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PIL import Image
 import io
 import tflite_runtime.interpreter as tflite
 
+app = FastAPI()
+
 CLASS_NAMES = [
     'CORN_Bacterial Leaf Streak',
     'CORN_Common_rust',
@@ -13,43 +15,35 @@ CLASS_NAMES = [
     'PEANUT_ALTERNARIA LEAF SPOT',
     'PEANUT_HEALTHY',
     'PEANUT_LEAF SPOT (EARLY AND LATE)',
-    'PEANUT_ROSETTE'
+    'PEANUT_ROSETTE',
+    'PEANUT_RUST'
 ]
 
-app = FastAPI()
-
-# Load model once at startup
+# Load model once
 interpreter = tflite.Interpreter(model_path="model.tflite")
 interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
+
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     image_bytes = await file.read()
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-
-    # Resize to correct model size
     image = image.resize((160, 160))
 
-    # Convert to numpy
     input_data = np.array(image, dtype=np.float32)
 
-    # Normalize
-    input_data = input_data / 255.0
-
-    # Add batch dimension
     input_data = np.expand_dims(input_data, axis=0)
 
-    # Run inference
     interpreter.set_tensor(input_details[0]['index'], input_data)
     interpreter.invoke()
 
-    output = interpreter.get_tensor(output_details[0]['index'])
+    output = interpreter.get_tensor(output_details[0]['index'])[0]
 
     pred_index = int(np.argmax(output))
-    confidence = float(np.max(output))
+    confidence = float(output[pred_index])
 
     return {
-    "disease": CLASS_NAMES[pred_index],
-    "confidence": confidence
+        "disease": CLASS_NAMES[pred_index],
+        "confidence": round(confidence, 4)
     }
